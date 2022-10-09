be178cf15c6e78f35d866581c909f07874b916fc
Vinsent Seigi <niseimono@gmail.com>
2022-10-09 22:42:23 +0300

    Blog (all 4 parts done)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -10,9 +11,12 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
 
+load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SESSION_KEY']
+# app.secret_key = '' #Required for sessions to work (Flask-Login); set in .env
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
