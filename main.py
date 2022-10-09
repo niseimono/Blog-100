ce795023200254ad2a6e654830c3ba246d04481a
Vinsent Seigi <niseimono@gmail.com>
2022-10-10 02:20:14 +0300

    SQLite -> Postgres (Heroku)

--- main.py
+++ main.py
@@ -21,8 +21,10 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') #from Heroku config vars (Postgres)
+if os.environ.get('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') #from Heroku config vars (Postgres)
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' #local DB
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
